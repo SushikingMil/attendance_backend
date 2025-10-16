8fca3723a23406806a914ec8dea23d5b3427fb50
SushikingMil <dipendenti@sushikingmilano.it>
2025-10-16 19:39:58 +0200

    Update start_server.py

--- start_server.py
+++ start_server.py
@@ -6,5 +6,5 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from src.main import app
 
 if __name__ == '__main__':
-port = int(os.environ.get('PORT', 5001))
-app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=True)
